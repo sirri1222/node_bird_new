9df158f13babdb1c8c75c69a4b38528dea4702e9
sirri1222 <hyeonji159753@gmail.com>
2024-01-05 20:24:27 +0900

    Fix: 레이아웃 수정

--- prepare/front/devhyun_twitter/src/component/SignupForm.jsx
+++ prepare/front/devhyun_twitter/src/component/SignupForm.jsx
@@ -1,15 +1,41 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import userInput from "../hooks/userInput";
-import { Form, Input } from "antd";
+import { Form, Input, Checkbox } from "antd";
+
 
 const SignupForm = () => {
   const [id, onChangeId] = userInput("");
   const [password, onChangePassword] = userInput("");
   const [nickname, onChangeNickname] = userInput("");
 
-  const onSubmit = useCallback(() => {}, []);
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+
+  const [TermCheck, setTermCheck] = useState("");
+  const [TermError, setTermError] = useState(false);
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const onChangeterm = useCallback((e) => {
+    setTermCheck(e.target.checked);
+    setTermError(false);
+  }, []);
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!TermCheck) {
+      return setTermError(true);
+    }
+  }, [password, passwordCheck, TermCheck]);
   return (
     <div>
       <title>회원가입</title>
@@ -23,11 +49,26 @@ const SignupForm = () => {
           <label htmlFor="user-password">패스워드</label>
           <br />
           <Input
+            type="password"
             value={password}
             name="user-password"
             onChange={onChangePassword}
             required
           />
+        </div>
+        <div>
+          <label htmlFor="user-passwordcheck">패스워드 확인</label>
+          <br />
+          <Input
+            type="password"
+            value={passwordCheck}
+            name="user-passwordcheck"
+            onChange={onChangePasswordCheck}
+            required
+          />
+          {passwordError && (
+            <ErrorMassage>비밀번호가 일치하지 않습니다.</ErrorMassage>
+          )}
         </div>
         <div>
           <label htmlFor="userid">닉네임</label>
@@ -38,6 +79,13 @@ const SignupForm = () => {
             onChange={onChangeNickname}
             required
           />
+        </div>
+        <div>
+          <Checkbox>개인정보 수집에 동의 합니다. </Checkbox>
+          {TermError && <ErrorMassage>필수 동의를 체크해 주세요</ErrorMassage>}
+        </div>
+        <div>
+          <button>가입하기</button>
         </div>
       </Form>
     </div>
