998c056af7129c57e5efd56a720dbfd21f080ec4
sirri1222 <hyeonji159753@gmail.com>
2024-01-19 18:59:07 +0900

    Fix: 빌드오류 수정

--- prepare/front/devhyun_twitter/src/app/page.js
+++ prepare/front/devhyun_twitter/src/app/page.js
@@ -1,10 +1,11 @@
+"use client";
 import PostCard from "../component/PostCard";
 import PostForm from "../component/PostForm";
 import Login from "../component/main/Login";
 import useStore from "../store/store";
+
 export default function Home() {
   const { posts } = useStore();
-
   const { username } = useStore();
   return (
     <>
